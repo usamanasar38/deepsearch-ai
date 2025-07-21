3b4b195a905a993a8337b3ac51ec2a69d793242c
Usama Nasar <usamanasar38@gmail.com>
2025-07-21 19:07:28 +0500

    fix: fix vitest timeout

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,10 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   test: {
     setupFiles: ["dotenv/config"],
+    testTimeout: 60_000,
+    sequence: {
+      concurrent: false, // Ensure tests run sequentially to avoid rate limit on free tier
+    },
   },
   plugins: [tsconfigPaths()],
 });
